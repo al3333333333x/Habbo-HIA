d28aeef4ef453a30fd5efed68adc79de8dbb6d07
al3333333333x <alexlimyuanshen@gmail.com>
2025-05-16 01:54:04 +0800

    Delete js/models directory

--- js/models/authModel.js
+++
@@ -1,8 +0,0 @@
-
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
-
-const auth = getAuth();
-
-export const registerUser = (email, password) => createUserWithEmailAndPassword(auth, email, password);
-export const loginUser = (email, password) => signInWithEmailAndPassword(auth, password);
-export const logoutUser = () => signOut(auth);
